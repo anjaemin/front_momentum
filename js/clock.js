654b3e65820b266b51b0758acff45b6b5868304c
ahnjaemin <woalse1014@gmail.com>
2022-11-16 19:14:18 +0900

    quotes, image 추가

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    const hour = new String(date.getHours()).padStart(2, "0");
+    const minutes = new String(date.getMinutes()).padStart(2, "0");
+    const second = new String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hour}:${minutes}:${second}`;
 }
 
-// setInterval(sayHello, 2000);
+getClock();
+setInterval(getClock, 1000);
